67b5268ce6082c7850a37148996f2a13955a5ef8
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-08-07 01:59:48 -0400

    cloudfront

--- script/deploy.sh
+++ script/deploy.sh
@@ -16,5 +16,7 @@ fi
 aws s3 sync --acl public-read --follow-symlinks --delete output "s3://${AWS_BUCKET}"
 # https://anands.github.io/blog/2018/11/18/serve-static-pages-on-s3-without-html-extension/
 aws s3 cp "s3://${AWS_BUCKET}/index" "s3://${AWS_BUCKET}/index" --content-type="text/html" --acl public-read
+# FIXME cloudfront won't respect content type
+aws s3 cp "s3://${AWS_BUCKET}/index" "s3://${AWS_BUCKET}/index.html" --content-type="text/html" --acl public-read
 aws s3 cp "s3://${AWS_BUCKET}/about" "s3://${AWS_BUCKET}/about" --content-type="text/html" --recursive --acl public-read
 aws s3 cp "s3://${AWS_BUCKET}/guides" "s3://${AWS_BUCKET}/guides" --content-type="text/html" --recursive --acl public-read
