45b8fed8363a775ac4bd02ab2e4965b01e8e52e0
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2021-08-07 01:53:26 -0400

    set content type

--- script/deploy.sh
+++ script/deploy.sh
@@ -14,3 +14,6 @@ fi
 ./script/build.sh
 
 aws s3 sync --acl public-read --follow-symlinks --delete output "s3://${AWS_BUCKET}"
+# https://anands.github.io/blog/2018/11/18/serve-static-pages-on-s3-without-html-extension/
+aws s3 cp "s3://${AWS_BUCKET}/index" "s3://${AWS_BUCKET}/index" --content-type="text/html"
+aws s3 cp "s3://${AWS_BUCKET}/guides" "s3://${AWS_BUCKET}/guides" --content-type="text/html" --recursive
